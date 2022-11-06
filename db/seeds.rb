64a790261c83e53c0d61d43295a3ed433532b514
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-11-06 18:25:34 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,5 +22,8 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
-
+review1 = Review.create(star_rating: 5, comment: "This is a great product", user_id: user1.id, product_id: product1.id)
+review2 = Review.create(star_rating: 10, comment: "I like it", user_id: user2.id, product_id: product2.id)
+review3 = Review.create(star_rating: 6, comment: "This is a nice product", user_id: user1.id, product_id: product3.id)
+review4 = Review.create(star_rating: 5, comment: "Awesome", user_id: user3.id, product_id: product2.id)
 puts "Seeding done!"
